ad1217824d20ae400f8dccc42118c635b96db6a8
Chris Romack <chris.romack@invitationhomes.com>
2022-06-27 20:08:40 -0500

    feat: create deployment record in ci/cd automation database
    jira: CAPI-150

---
+++ .github/actions/create-deployment/index.js
@@ -0,0 +1,31 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+import axios from 'axios';
+
+const run = async () => {
+  const apiToken = core.getInput('api-token');
+  const environment = core.getInput('environment');
+  const version = core.getInput('version');
+  const isSuccessful = core.getInput('isSuccessful');
+  core.info(`record-deployment() => ${version} was deployed to ${environment}`);
+
+  await axios.post(
+    'https://api.invitationhomes.com/ci-cd/v1/deployments',
+    {
+      repository: github.context.payload.repository.name,
+      environment,
+      version,
+      commit: github.context.sha,
+      timestamp: new Date().toISOString(),
+      isSuccessful
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${apiToken}`
+      }
+    })
+    .then(response => core.info(`record-deployment() => status: ${response.status}`))
+    .catch(error => core.error(`record-deployment() => error: ${error.message}`));
+}
+
+run().catch(error => core.setFailed(error.message));
