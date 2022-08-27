a081f4e652c3e05ffc8e96ec469b1c8d35066c9e
Chris Romack <chris.romack@invitationhomes.com>
2022-08-27 09:09:28 -0500

    feat: add 'include-build-metadata' input to the create-git-tag action
    The create-git-action action will create a tag in git base upon the version
    found in package.json.
    
    Previously, this action would always include a build-metadata suffix in the
    generated tag (pre release tags).  This change will update it so that this
    action will only include that metadata if the include-build-metadata input is
    set to true.
    
    This will allow us to re-use this action for creating production release
    tags as well.
    
    jira: ENG-229

--- .github/actions/create-commit-tag/dist/index.js
+++ .github/actions/create-commit-tag/dist/index.js
@@ -8925,8 +8925,11 @@ const run = async () => {
   const packageJson = fs.readFileSync('./package.json', 'utf-8');
   const parsed = JSON.parse(packageJson);
   const shortSha = github.context.sha.substr(0, 7);
-  const tagName = `${parsed.version}-${github.context.runNumber}-${shortSha}`;
   const githubToken = core.getInput('github-token');
+  const includeBuildMetadata = core.getInput('include-build-metadata');
+  const tagName = (includeBuildMetadata === 'true')
+    ? `${parsed.version}-${github.context.runNumber}-${shortSha}`
+    : parsed.version;
   const octokit = github.getOctokit(githubToken);
 
   const existingReference = await octokit.rest.git.getRef({
